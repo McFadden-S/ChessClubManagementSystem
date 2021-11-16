1eeaaa56a91413aa16b54cba6baacfb1be3dd866
Jun Yi Goh <jun.goh@kcl.ac.uk>
2021-11-16 22:43:11 +0000

    Create a log_in form inside the log_in view.
    - Add url to log_in page
    - Link the home page to the log_in page
    - Add a log_in view
    - Add log_in.html
    - Add a LogInForm inside forms.py
    - Add LogInForm to log_in view and template

---
+++ clubs/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class LogInForm(forms.Form):
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput())
--- clubs/views.py
+++ clubs/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .forms import LogInForm
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def log_in(request):
+    form = LogInForm()
+    return render(request, 'log_in.html', {'form': form})
