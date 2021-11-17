1fc2269d8fc2c7e0eff1d04cd8f980b2d4f90b0c
Manivannan Prushorth <manivanann.prushorth@kcl.ac.uk>
2021-11-17 20:43:34 +0000

    Add override of create_user method in models.py

--- clubs/models.py
+++ clubs/models.py
@@ -1,9 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self,email,first_name,last_name,bio,chess_experience,personal_statement,password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            bio=first_name,
+            chess_experience=chess_experience,
+            personal_statement=personal_statement,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractUser):
-    # username = None
+    username = None
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
     email = models.EmailField(unique=True, blank = False)
@@ -25,5 +44,6 @@ class User(AbstractUser):
     )
 
     personal_statement = models.CharField(max_length=100, blank=True)
+    objects = UserManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name','last_name','bio','chess_experience','personal_statement']
