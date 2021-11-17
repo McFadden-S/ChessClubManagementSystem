2c0ca6ae3c5ffe2446e4fc5b82a823e222700064
Manivannan Prushorth <manivanann.prushorth@kcl.ac.uk>
2021-11-17 00:07:23 +0000

    Create the User Model

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank = False)
+    # gravatar =
+    bio = models.CharField(max_length=100, blank=True)
+
+    BEGINNER = 'BG'
+    INTERMEDIATE = 'IM'
+    ADVANCED = 'AV'
+    CHESS_EXPERIENCE_CHOICES = [
+        (BEGINNER, 'Beginner'),
+        (INTERMEDIATE, 'Intermediate'),
+        (ADVANCED, 'Advanced'),
+    ]
+    chess_experience = models.CharField(
+        max_length=2,
+        choices=CHESS_EXPERIENCE_CHOICES,
+        default=BEGINNER
+    )
