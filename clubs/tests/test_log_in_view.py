bc60f850bcb5d570282fca2b2baf9893f8bf61f4
Jun Yi Goh <jun.goh@kcl.ac.uk>
2021-11-19 16:18:01 +0000

    Finish the log_in view.
    - Redirects the user to the home page after a successful log in.
    - Add tests for the log_in view.
    - Show a message that tells the user if the log in is unsuccessful.

---
+++ clubs/tests/test_log_in_view.py
@@ -0,0 +1,58 @@
+"""Unit tests of the log in view"""
+from django.contrib import messages
+from django.urls import reverse
+from django.test import TestCase
+from clubs.forms import LogInForm
+from clubs.models import User
+
+class LogInViewTestCase(TestCase):
+    """Unit tests of the log in view"""
+    def setUp(self):
+        self.url = reverse('log_in')
+        User.objects.create_user(email='orangutan@example.org',
+            first_name='orang',
+            last_name='utan',
+            bio='I am a smart orangutan',
+            chess_experience='Beginner',
+            personal_statement='I would love to learn how to play chess',
+            password='CorrectPassword123'
+        )
+
+    def test_log_in_url(self):
+        self.assertEqual(self.url, '/log_in/')
+
+    def test_get_log_in(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'log_in.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, LogInForm))
+        self.assertFalse(form.is_bound)
+        messages_list = lost(response.context['messages'])
+        self.assertEqual(len(messages_list), 0)
+
+    def test_unsuccessful_log_in(self):
+        self.form_input = {'email': 'orangutan@example.org', 'password': 'WrongPassword123'}
+        response = self.client.post(self.url, form_input)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'log_in.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, LogInForm))
+        self.assertFalse(form.is_bound)
+        self.assertFalse(self._is_logged_in())
+        messages_list = lost(response.context['messages'])
+        self.assertEqual(len(messages_list), 1)
+        self.assertEqual(messages_list[0].level, messages.ERROR)
+
+    def test_successful_log_in(self):
+        self.form_input = {'email': 'orangutan@example.org', 'password': 'CorrectPassword123'}
+        response = self.client.post(self.url, form_input, follow=True)
+        self.assertTrue(self._is_logged_in())
+        response_url = reverse('home')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'home.html')
+        messages_list = lost(response.context['messages'])
+        self.assertEqual(len(messages_list), 0)
+
+    def _is_logged_in(self):
+        return '_auth_user_id' in self.client.session.keys()
--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from .forms import SignUpForm
 from .forms import LogInForm
@@ -11,5 +13,16 @@ def sign_up(request):
     return render(request,'sign_up.html',{'form': form})
 
 def log_in(request):
+    if request.method == 'POST':
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                login(request, user)
+                redirect_url = 'home'
+                return redirect(redirect_url)
+        messages.add_message(request, messages.ERROR, "The credentials provided were invalid")
     form = LogInForm()
     return render(request, 'log_in.html', {'form': form})
