80d4f683522ab12510ee0f47926700ba17f7a029
Aarjav Jain <aarjav02@gmail.com>
2021-11-19 19:31:24 +0000

    Add Club db to admin interface

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Club
 
 # Register your models here.
 @admin.register(User)
@@ -8,4 +8,12 @@ class UserAdmin(admin.ModelAdmin):
 
     list_display = [
         'email', 'first_name', 'last_name', 'chess_experience', 'is_active',
+    ] 
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for clubs."""
+
+    list_display = [
+        'user', 'club_name', 'authorization',
     ] 
