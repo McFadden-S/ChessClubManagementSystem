376d27dd3f57f5ca2158833300e0c33a1d769362
Manivannan Prushorth <manivanann.prushorth@kcl.ac.uk>
2021-11-21 11:27:18 +0000

    Rewrite show_applicant using fixture

--- clubs/tests/views/test_show_applicant_view.py
+++ clubs/tests/views/test_show_applicant_view.py
@@ -3,18 +3,17 @@ from clubs.models import User,Club
 from django.urls import reverse
 
 class ShowApplicantViewTestCase(TestCase):
+
+    fixtures = [
+        'clubs/tests/fixtures/default_user.json',
+    ]
+
     def setUp(self):
-        self.user =  User.objects.create_user(
-            email='bobsmith@example.com',
-            first_name = 'Bob',
-            last_name = 'Smith',
-            bio='Hi',
-            chess_experience='AV',
-            personal_statement = 'I am Orangutan',
-            password="Orangutan123"
+        self.user = User.objects.get(email='bobsmith@example.org')
+        self.club = Club.objects.create(
+            user=self.user, authorization='OW'
         )
-        Club.objects.create(user=self.user)
-        self.target_user = User.objects.get(email='bobsmith@example.com')
+        self.target_user = User.objects.get(email='bobsmith@example.org')
         self.url = reverse('show_applicant', kwargs={'applicant_id': self.target_user.id})
 
     def test_show_user_url(self):
@@ -26,5 +25,5 @@ class ShowApplicantViewTestCase(TestCase):
         self.assertTemplateUsed(response, 'show_applicant.html')
         self.assertContains(response, "Bob Smith")
         self.assertContains(response, "Hi")
-        self.assertContains(response, "AV")
+        self.assertContains(response, "BG")
         self.assertContains(response, "I am Orangutan")
