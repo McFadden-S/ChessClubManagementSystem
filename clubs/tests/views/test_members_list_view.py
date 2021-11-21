aedb620b366f1904d4d55a74cf548f61b05ef85b
Jun Yi Goh <jun.goh@kcl.ac.uk>
2021-11-21 22:34:58 +0000

    Add tests for the search bar in members_list

--- clubs/tests/views/test_members_list_view.py
+++ clubs/tests/views/test_members_list_view.py
@@ -9,12 +9,22 @@ class membersListViewTestCase(TestCase):
 
     fixtures = [
         'clubs/tests/fixtures/default_user.json',
+        'clubs/tests/fixtures/secondary_user.json',
+        'clubs/tests/fixtures/tertiary_user.json'
     ]
 
     def setUp(self):
         self.user = User.objects.get(email='bobsmith@example.org')
+        self.secondary_user = User.objects.get(email='bethsmith@example.org')
+        self.tertiary_user = User.objects.get(email='johnsmith@example.org')
         self.club = Club.objects.create(
             user=self.user, authorization='OW'
+        )
+        Club.objects.create(
+            user=self.secondary_user, authorization='ME'
+        )
+        Club.objects.create(
+            user=self.tertiary_user, authorization='ME'
         )
         self.url = reverse('members_list')
 
@@ -36,6 +46,34 @@ class membersListViewTestCase(TestCase):
         redirect_url = reverse_with_next('log_in', self.url)
         response = self.client.post(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    def test_search_bar_to_filter_list(self):
+        self.client.login(email=self.user.email, password='Password123')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'members_list.html')
+        self.assertContains(response, 'John Smith')
+        self.assertContains(response, 'Beth Smith')
+        search_bar = 'Beth'
+        response = self.client.post(self.url, {'searched_letters': search_bar})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'members_list.html')
+        self.assertContains(response, 'Beth Smith')
+        self.assertNotContains(response, 'John Smith')
+
+    def test_empty_search_bar(self):
+        self.client.login(email=self.user.email, password='Password123')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'members_list.html')
+        self.assertContains(response, 'John Smith')
+        self.assertContains(response, 'Beth Smith')
+        search_bar = ''
+        response = self.client.post(self.url, {'searched_letters': search_bar})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'members_list.html')
+        self.assertContains(response, 'Beth Smith')
+        self.assertContains(response, 'John Smith')
 
 
     #TODO Once Log In redirects to another view members only decorator and this test can be UNCOMMENTED
