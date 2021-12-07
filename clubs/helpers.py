da9c4aae675addab5c9a5294b00c11517b4a4f26
Manivannan Prushorth <manivanann.prushorth@kcl.ac.uk>
2021-12-07 15:50:07 +0000

    Modify comment in removeclub

--- clubs/helpers.py
+++ clubs/helpers.py
@@ -191,7 +191,7 @@ def remove_clubs(user, clubs):
             if count_applicants_in_club + 1 == count_all_users_in_club:
                 club.delete()
             elif get_count_of_specific_user_in_club(club, 'OW') == 1:
-                # at least one member officer or owner in addition  to applicants and owner
+                # at least one member or at least 1 officer in addition  to applicants and owner
                 return (False, club.id)
 
     return (True,0)
