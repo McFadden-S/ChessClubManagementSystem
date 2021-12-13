5c750adad5b7ee672eeae1a4cfdc1be27fa6dfc3
Musa Ghafoor <k20073759@kcl.ac.uk>
2021-12-13 18:36:17 +0000

    Update test_waiting_list_view.py

--- clubs/tests/views/test_waiting_list_view.py
+++ clubs/tests/views/test_waiting_list_view.py
@@ -9,7 +9,6 @@ from django.contrib import messages
 
 # Used this from clucker project with some modifications
 class ApplicantListViewTestCase(TestCase, LogInTester, NavbarTesterMixin):
-    # Import default users and club.
     fixtures = [
         'clubs/tests/fixtures/default_user.json',
         'clubs/tests/fixtures/other_users.json',
@@ -17,7 +16,6 @@ class ApplicantListViewTestCase(TestCase, LogInTester, NavbarTesterMixin):
     ]
 
     def setUp(self):
-        # Setup
         self.owner = User.objects.get(email='harrysmith@example.org')
         self.officer = User.objects.get(email='bobsmith@example.org')
         self.applicant = User.objects.get(email='bethsmith@example.org')
@@ -41,12 +39,10 @@ class ApplicantListViewTestCase(TestCase, LogInTester, NavbarTesterMixin):
 
     def test_waiting_list_url(self):
         """"Test for the waiting list url."""
-
         self.assertEqual(self.url, f'/{self.club.id}/waiting_list/')
 
     def test_waiting_list_content_when_signed_in_as_applicant(self):
         """"Test content of waiting list for applicant"""
-
         self.client.login(email=self.applicant.email, password='Password123')
         self.assertTrue(self._is_logged_in())
         response = self.client.get(self.url)
@@ -54,44 +50,34 @@ class ApplicantListViewTestCase(TestCase, LogInTester, NavbarTesterMixin):
         self.assert_main_navbar(response)
         self.assertEqual(response.status_code, 200)
 
-    # Redirects
+    """Test waiting list redirects to members list when signed in as user"""
 
     def test_waiting_list_redirects_to_members_list_when_signed_in_as_member(self):
-        """Test redirect to member list for member."""
-
-        # shows message You are not an applicant
         self.client.login(email=self.member.email, password='Password123')
         self.assertTrue(self._is_logged_in())
         response = self.client.get(self.url)
         redirect_url = reverse('members_list', kwargs={'club_id': self.club.id})
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
-
         # messages_list = list(response.context['messages'])
         # self.assertEqual(len(messages_list), 1)
         # self.assertEqual(messages_list[0].level, messages.ERROR)
 
     def test_waiting_list_redirects_to_members_list_when_signed_in_as_officer(self):
-        """Test redirect to member list for officer."""
-
         self.client.login(email=self.officer.email, password='Password123')
         self.assertTrue(self._is_logged_in())
         response = self.client.get(self.url)
         redirect_url = reverse('members_list', kwargs={'club_id': self.club.id})
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
-
         # messages_list = list(response.context['messages'])
         # self.assertEqual(len(messages_list), 1)
         # self.assertEqual(messages_list[0].level, messages.ERROR)
 
     def test_waiting_list_redirects_to_members_list_when_signed_in_as_owner(self):
-        """Test redirect to member list for owner."""
-
         self.client.login(email=self.owner.email, password='Password123')
         self.assertTrue(self._is_logged_in())
         response = self.client.get(self.url)
         redirect_url = reverse('members_list', kwargs={'club_id': self.club.id})
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
-
         # messages_list = list(response.context['messages'])
         # self.assertEqual(len(messages_list), 1)
         # self.assertEqual(messages_list[0].level, messages.ERROR)
