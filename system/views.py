ea21f202c642339a68def9b84b67a280fa3513d4
Shae McFadden <shae.mcfadden@kcl.ac.uk>
2021-12-14 10:15:51 +0000

    Clean Up

--- system/views.py
+++ system/views.py
@@ -1,13 +1,5 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
 
-class PageNotFoundView(TemplateView):
-    """ View for when page is not found """
-
-    template_name = '404.html'
-
-    def render(request, exception):
-        return render(request, template_name, status=404)
-
 def page_not_found_view(request, exception):
     return render(request, '404.html', status=404)
